7e9b95581698b26f8373ddda57da319f588ffdd7
Fishon Amos <amosfishon@gmail.com>
2024-09-04 17:57:26 +0100

    code refac

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+  const LandRegistry = await hre.ethers.getContractFactory("LandRegistry");
+  const landRegistry = await LandRegistry.deploy();
+
+  await landRegistry.deployed();
+
+  console.log("LandRegistry deployed to:", landRegistry.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
