736493759c3cd07fd05f8b62c918983c28f0d896
Fishon Amos <43862685+fishonamos@users.noreply.github.com>
2024-09-05 08:02:18 +0100

    Revert "server.ts"

--- backend/src/app.js
+++ backend/src/app.js
@@ -71,4 +71,6 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
