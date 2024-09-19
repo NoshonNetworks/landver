8338ca8cb7a46ec462a71c8bdbbba555bd0cc5db
Fishon Amos <43862685+fishonamos@users.noreply.github.com>
2024-09-19 12:35:22 +0100

    Update landRoutes.js

--- backend/routes/landRoutes.js
+++ backend/routes/landRoutes.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const router = express.Router();
-const landController = require('../controllers/landController');
+// const express = require('express');
+// const router = express.Router();
+// const landController = require('../controllers/landController');
 
-// Remove any authentication middleware for these routes
-router.post('/register', landController.registerLand);
-router.get('/', (req, res, next) => {
-  console.log('Received GET request for all lands');
-  landController.getAllLands(req, res, next);
-});
-router.get('/:id', landController.getLandById);
-router.get('/:id/verify', landController.verifyLand);
+// // Remove any authentication middleware for these routes
+// router.post('/register', landController.registerLand);
+// router.get('/', (req, res, next) => {
+//   console.log('Received GET request for all lands');
+//   landController.getAllLands(req, res, next);
+// });
+// router.get('/:id', landController.getLandById);
+// router.get('/:id/verify', landController.verifyLand);
 
-module.exports = router;
+// //Todo: docs, api
+
+
+// module.exports = router;
