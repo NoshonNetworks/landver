6b91663508ee0a3acfdb66ce907c6cbdc3699e50
Solomon Emmanuel <soulex04292003@gmail.com>
2024-10-17 11:36:16 +0100

    fix:200~Added frontend as a regular directory

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,116 @@
+// app/about/page.tsx
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  Home,
+  FileBadgeIcon as NFT,
+  CheckCircle,
+  User,
+ FileText as Document,
+  ShieldCheck,
+  Eye,
+  FileSearch,
+  Coins as CurrencyDollar,
+} from "lucide-react";
+
+const AboutPage = () => {
+  return (
+    <div className="mx-auto p-8 bg-gradient-to-b from-green-100 to-white shadow-lg rounded-lg">
+      <div className="flex items-center mb-6">
+        <Link href="/">
+          <Home className="text-3xl  mr-2 bg-green-500 text-[#ffe] w-[50px] h-[50px] p-3 rounded-full" />
+        </Link>
+        <h1 className="text-4xl font-light text-gray-800">About Landver</h1>
+      </div>
+      <Image
+        src="/images/landver_logo_WHITE.jpg"
+        alt="Landver Logo"
+        width={100}
+        height={100}
+        className="mx-auto mb-6 rounded-md shadow-md"
+      />
+      <h2 className="text-3xl text-gray-800 mb-4 text-center p-3 border-2 border-green-600 border-l-0 border-r-0">
+        How It Works
+      </h2>
+
+      <p className="text-lg text-gray-700 mb-4">
+        At Landver, we recognize the critical challenges associated with land
+        ownership and management in today’s digital landscape. Asset-related
+        fraud remains a significant concern for landowners, inspectors, and
+        regulatory bodies alike. Our mission is to combat these issues head-on
+        by providing a secure, user-friendly platform that not only simplifies
+        the land registration process but also enhances transparency and trust
+        among all stakeholders involved. We leverage onchain technology to
+        create a robust framework for land management, allowing for a seamless
+        interaction between landowners and land inspectors.
+      </p>
+
+      <p className="text-lg text-gray-700 mb-4">
+        The Landver platform serves as a dynamic hub where landowners can
+        register their properties with confidence, while inspectors conduct
+        thorough reviews to ensure compliance and accuracy. Here’s a deeper dive
+        into how our process works, step by step:
+      </p>
+
+      <ul className="list-disc list-inside mb-4 space-y-2">
+        <li className="text-lg text-gray-700">
+          <strong>
+            <User className="inline text-green-600" size={24} /> Land Owners:
+          </strong>{" "}
+          The journey begins with landowners who wish to register their
+          properties on our platform. Users can easily sign up and submit
+          essential documentation, such as proof of ownership{" "}
+          <Document className="inline text-blue-500" size={24} />, land surveys,
+          and other relevant information. Once submitted, the registration
+          details are securely recorded on our blockchain{" "}
+          <ShieldCheck className="inline text-gray-700" size={24} />, creating a
+          permanent and tamper-proof record of ownership.
+        </li>
+
+        <li className="text-lg text-gray-700">
+          <strong>
+            <Eye className="inline text-yellow-500" size={24} /> Land
+            Inspectors:
+          </strong>{" "}
+          After a landowner submits their registration, our certified inspectors
+          step in to perform a meticulous review{" "}
+          <FileSearch className="inline text-purple-500" size={24} /> of the
+          submitted documents and the physical property itself. This phase is
+          essential for verifying the authenticity of the ownership claims.
+        </li>
+
+        <li className="text-lg text-gray-700">
+          Once the inspection is successfully completed, the landowner receives
+          an NFT <NFT className="inline text-blue-900" size={30} />{" "}
+          (Non-Fungible Token) that serves as a digital proof of ownership. This
+          NFT encapsulates all relevant details about the land, including
+          ownership history, inspection records, and any associated
+          transactions.
+        </li>
+      </ul>
+
+      <p className="text-lg text-gray-700 mb-4">
+        Furthermore, the Landver platform integrates advanced technology to
+        ensure that every interaction is transparent and verifiable{" "}
+        <CheckCircle className="inline text-green-600" size={24} />. Users can
+        track the status of their registrations, view inspection results, and
+        access their NFTs at any time. This level of transparency is
+        unprecedented in the realm of land management and significantly
+        mitigates the risks associated with traditional processes.
+      </p>
+
+      <p className="text-lg text-gray-700 mb-4">
+        Our use of NFTs revolutionizes how land ownership is perceived and
+        managed, creating new avenues for economic engagement and stability{" "}
+        <CurrencyDollar className="inline text-green-700" size={24} />.
+      </p>
+
+      <Link href="/" className="text-lg text-blue-600 hover:underline mb-4">
+        Back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ frontend/src/app/components/NavBar.tsx
@@ -0,0 +1,174 @@
+"use client";
+import React, { useState } from "react";
+import { FaGithub } from "react-icons/fa";
+import Image from "next/image";
+import { SunDim, Search } from "lucide-react";
+import { FaX } from "react-icons/fa6";
+
+const Header: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isSearchIconClicked, setIsSearchIconClicked] =
+    useState<boolean>(false);
+
+  return (
+    <header className="text-black">
+      <nav className="flex items-center justify-between border">
+        <div className="flex items-center">
+          <div className="md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="focus:outline-none"
+            >
+              <svg
+                className="w-8 h-8"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                {isOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+          <Image
+            src="/images/LANDVER-NO-BG.png"
+            alt="Landver Logo"
+            className="w-[3.5em] ml-3"
+            width={56} 
+            height={56} 
+          />
+          {isSearchIconClicked ? (
+            <h1>Home</h1> // Changed from Home icon to text for simplicity
+          ) : (
+            <ul>
+              <li className="hover:text-gray-400 font-bold">Home</li>
+            </ul>
+          )}
+          <ul className="hidden md:flex space-x-6 items-center ml-2">
+            <li className="hover:text-gray-400">API</li>
+            <li className="hover:text-gray-400">GUIDES</li>
+          </ul>
+        </div>
+        <div className="flex items-center p-2">
+          <div className="flex items-center space-x-6">
+            <a
+              href="https://github.com/NoshonNetworks/landver/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hidden md:block"
+            >
+              <FaGithub className="w-5 h-5 inline mr-2" />
+              <span>Github</span>
+            </a>
+          </div>
+          <div>
+            <button>
+              <SunDim className="p-3 h-[55px] hidden md:block" size={60} />
+            </button>
+          </div>
+          <div>
+            <input
+              type="search"
+              className="border-2 border-gray-800 p-3 bg-inherit rounded-lg hidden md:block"
+              placeholder="Search"
+            />
+            {isSearchIconClicked ? (
+              <div className="flex items-center">
+                <input
+                  type="text"
+                  placeholder="Search Docs"
+                  className={`p-3 rounded-lg bg-inherit text-black border-2 border-black placeholder:text-center w-[80%] block ${
+                    isSearchIconClicked
+                      ? "sm:absolute sm:top-2 sm:left-1 md:block lg:block"
+                      : ""
+                  } md:w-fit`}
+                />
+                <FaX
+                  onClick={() => setIsSearchIconClicked(false)}
+                  className="p-4 rounded text-gray-700"
+                  size={45}
+                />
+              </div>
+            ) : (
+              <Search
+                className="h-[30px] md:hidden"
+                size={30}
+                onClick={() => setIsSearchIconClicked(!isSearchIconClicked)}
+              />
+            )}
+          </div>
+        </div>
+      </nav>
+
+      {/* Mobile Menu */}
+      <div
+        className={`md:hidden fixed top-0 left-0 w-[80%] h-full bg-white transition-transform duration-300 ease-in-out ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="flex items-center justify-around border">
+          <div className="grid grid-cols-2 items-center justify-evenly p-3 py-0">
+            <Image
+              src="/images/LANDVER_LOGO_WHITE-removebg-preview.png"
+              alt="Landver Logo"
+              className="w-[3.5em]"
+              width={56} 
+              height={56} 
+            />
+            <ul>
+              <li className="hover:text-gray-400 font-bold">Home</li>
+            </ul>
+          </div>
+          <div>
+            <button>
+              <SunDim className="h-[77px]" size={40} />
+            </button>
+          </div>
+          <FaX onClick={() => setIsOpen(false)} className="h-[75px]" />
+        </div>
+
+        <div className="flex flex-col items-center justify-center h-full space-y-6 text-2xl">
+          <a href="#" onClick={() => setIsOpen(false)}>
+            API
+          </a>
+          <a href="#" onClick={() => setIsOpen(false)}>
+            GUIDES
+          </a>
+          <select
+            name=""
+            id=""
+            className="border-2 border-black rounded p-2 bg-inherit"
+          >
+            <option value="">0.3</option>
+            <option value="">0.2</option>
+            <option value="">0.1</option>
+          </select>
+
+          <a
+            href="https://github.com/NoshonNetworks/landver/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-black"
+          >
+            <FaGithub className="w-5 h-5" />
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
