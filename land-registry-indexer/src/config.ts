0ebeebb26b42c2ec230ac6117ed1dc1d8d9467e4
fishonamos <amosfishon@gmail.com>
2024-12-01 21:52:39 +0100

    indexer

--- land-registry-indexer/src/config.ts
+++ land-registry-indexer/src/config.ts
@@ -5,5 +5,5 @@ export const config = {
   startingBlock: Number(process.env.STARTING_BLOCK || 0),
   landRegistryAddress: process.env.LAND_REGISTRY_ADDRESS || '',
   pgConnection: process.env.DATABASE_URL || '',
-  apibaraUrl: process.env.APIBARA_URL || 'https://goerli.starknet.a5a.ch',
+  apibaraUrl: process.env.APIBARA_URL || '',
 }; 
