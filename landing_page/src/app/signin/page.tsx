90a285f1c1abe7c82821f1c2b94946247104acb3
Solomon Emmanuel <soulex04292003@gmail.com>
2024-12-02 20:04:36 +0100

    feat:impl auth

--- landing_page/src/app/signin/page.tsx
+++ landing_page/src/app/signin/page.tsx
@@ -1,11 +1,46 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import Button  from '../components/Button/Button';
-import { Input } from '../components/Input/Input';
-
+import { useState } from "react";
+import { toast } from "react-toastify";
+import Button from "../components/Button/Button";
+import { Input } from "../components/Input/Input";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to sign in");
+      }
+
+      toast.success("Signed in successfully!");
+      setTimeout(() => {
+        window.location.href = "https://demo.landver.net";
+      }, 1000);
+    } catch (error) {
+      console.error(error);
+      console.log(error instanceof Error ? error.message : "Failed to sign in");
+      toast.error(error instanceof Error ? error.message : "Failed to sign in");
+    } finally {
+      setLoading(false);
+      
+    }
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#f5f5f5] text-[#1f1f1f] px-4">
@@ -17,37 +52,48 @@ export default function LoginPage() {
           </p>
         </div>
 
-        <form className="mt-8 space-y-6">
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
           <Input
             type="text"
             placeholder="Enter Email / Phone No"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
           />
 
           <Input
-            type={showPassword ? 'text' : 'password'}
+            type={showPassword ? "text" : "password"}
             placeholder="Passcode"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
             rightElement={
               <button
                 type="button"
                 onClick={() => setShowPassword(!showPassword)}
                 className=""
               >
-                {showPassword?  'Hide' : 'Show'}
+                {showPassword ? "Hide" : "Show"}
               </button>
             }
           />
           <div className="text-xs font-[500]">
-            <span className="text-[#797979]">Don&apos;t have an account yet? </span>
+            <span className="text-[#797979]">
+              Don&apos;t have an account yet?{" "}
+            </span>
             <a href="/signup" className="text-[#000000]">
               Register now!
             </a>
           </div>
 
-          <Button classname='w-full !text-lg !font-[600]'>
-            Sign in
+          <Button classname="w-full !text-lg !font-[600]" disabled={loading}>
+            {loading ? "Signing in..." : "Sign in"}
           </Button>
         </form>
       </div>
+      <div className="absolute bottom-0 left-0 w-full">
+        <ToastContainer theme="colored" />
+      </div>
     </div>
-  )
+  );
 }
--- landing_page/src/app/signup/page.tsx
+++ landing_page/src/app/signup/page.tsx
@@ -1,16 +1,82 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import Button  from '../components/Button/Button';
-import { Input } from '../components/Input/Input';
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '../components/Select/Select';
+import { useState } from "react";
+import Button from "../components/Button/Button";
+import { Input } from "../components/Input/Input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../components/Select/Select";
+import { api } from "../lib/axios";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
 
 export default function SignupPage() {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    walletAddress: "",
+    role: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+
+    if (formData.password !== formData.confirmPassword) {
+      toast.error("Passwords do not match");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const response = await api.post("/auth/register", {
+        email: formData.email,
+        passcode: formData.password,
+        walletAddress: formData.walletAddress,
+        userType: formData.role,
+      });
+
+      localStorage.setItem("token", response.data.token);
+      toast.success("Registration successful!");
+      setTimeout(() => {
+        window.location.href = "https://demo.landver.net";
+      }, 1000);
+    } catch (err: any) {
+      toast.error(err.response?.data?.message || "An error occurred");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleRoleChange = (value: string) => {
+    setFormData({
+      ...formData,
+      role: value,
+    });
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#f5f5f5] text-[#1f1f1f] px-4">
+      <ToastContainer theme="colored" />
       <div className="max-w-md w-full space-y-8">
         <div className="text-center">
           <h1 className="text-[40px] font-bold">Let&apos;s set up</h1>
@@ -19,66 +85,89 @@ export default function SignupPage() {
           </p>
         </div>
 
-        <form className="mt-8 space-y-6">
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
           <Input
+            name="email"
             type="text"
+            value={formData.email}
+            onChange={handleChange}
+            required
             placeholder="Enter Email / Phone No"
           />
 
           <Input
-            type={showPassword ? 'text' : 'password'}
+            name="password"
+            type={showPassword ? "text" : "password"}
+            value={formData.password}
+            onChange={handleChange}
+            required
             placeholder="Passcode"
             rightElement={
               <button
                 type="button"
                 onClick={() => setShowPassword(!showPassword)}
-                className=""
               >
-                {showPassword?  'Hide' : 'Show'}
+                {showPassword ? "Hide" : "Show"}
               </button>
             }
           />
-          
+
           <Input
-            type={showConfirmPassword ? 'text' : 'password'}
+            name="confirmPassword"
+            type={showConfirmPassword ? "text" : "password"}
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
             placeholder="Confirm passcode"
             rightElement={
               <button
                 type="button"
                 onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                className=""
               >
-                {showConfirmPassword?  'Hide' : 'Show'}
+                {showConfirmPassword ? "Hide" : "Show"}
               </button>
             }
           />
+
           <Input
+            name="walletAddress"
             type="text"
+            value={formData.walletAddress}
+            onChange={handleChange}
+            required
             placeholder="Wallet address"
           />
-          <Select>
+
+          <Select onValueChange={handleRoleChange}>
             <SelectTrigger className="w-full">
-              <SelectValue placeholder="Land owner"/>
+              <SelectValue placeholder="Select role" />
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
-                <SelectItem value="Inspector">Inspector</SelectItem>
-                <SelectItem value="Land buyer">Land buyer</SelectItem>
+                <SelectItem value="landowner">Land owner</SelectItem>
+                <SelectItem value="inspector">Inspector</SelectItem>
+                <SelectItem value="landbuyer">Land buyer</SelectItem>
               </SelectGroup>
             </SelectContent>
           </Select>
+
           <div className="text-xs font-[500]">
             <span className="text-[#797979]">Already have an account? </span>
             <a href="/signin" className="text-[#000000]">
-              Sing in!
+              Sign in!
             </a>
           </div>
 
-          <Button classname='w-full !text-lg !font-[600]'>
-            Sign up
+          <Button
+            classname={`w-full !text-lg !font-[600] ${
+              loading ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            disabled={loading}
+          >
+            {loading ? "Signing up..." : "Sign up"}
           </Button>
         </form>
       </div>
     </div>
-  )
+  );
 }
