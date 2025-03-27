afada2e2c897dfbc1e331d26e63ac15b348a3cd4
Emmanuel <emmanuelogheneovo17@gmail.com>
2025-03-27 15:50:53 +0100

    Reverted unintended changes as requested

--- app/client/app/components/utils/Connector.tsx
+++ app/client/app/components/utils/Connector.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect } from "react";
 import WalletModal from "../ui/walletModal";
-import { useAppContext } from "@/app/context/appContext";
+import { useAppContext } from "../../../app/context/appContext";
 import { useBalance } from "@starknet-react/core";
 import { useRouter } from "next/navigation";
 import Button from "../ui/button";
