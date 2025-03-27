e25bdd72482d9fb98cfe47cba82cef9184901454
Emmanuel <emmanuelogheneovo17@gmail.com>
2025-03-27 16:02:04 +0100

    Reverted unintended changes as requested

--- app/client/app/components/utils/Connector.tsx
+++ app/client/app/components/utils/Connector.tsx
@@ -1,15 +1,11 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import WalletModal from "../ui/walletModal";
 import { useAppContext } from "../../../app/context/appContext";
 import { useBalance } from "@starknet-react/core";
 import { useRouter } from "next/navigation";
 import Button from "../ui/button";
 
-function parseAddress(addr: string | undefined) {
-  if (!addr) return "";
-  return ` ${addr?.slice(0, 8)}...${addr.slice(addr.length - 8, addr.length)}`;
-}
 const Connector: React.FC = () => {
   const { disconnectWallet, address, status } = useAppContext();
   const { data } = useBalance({ address: address as "0x" });
@@ -17,6 +13,7 @@ const Connector: React.FC = () => {
 
   console.log(data);
   console.log(status);
+
   return (
     <div className="p-6 text-center">
       {status === "connected" && (
@@ -33,6 +30,10 @@ const Connector: React.FC = () => {
             onClick={() => router.push("/dashboard/inspector")}
           >
             Inspector
+          </Button>
+          {/* Added disconnect button */}
+          <Button variant="ghost" onClick={disconnectWallet}>
+            Disconnect
           </Button>
         </div>
       )}
